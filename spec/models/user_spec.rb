441029657461573446e9a136fcb4cc1da24dcb28
Devin Bombay <d.bombay@live.com>
2018-03-28 14:58:55 -0400

    all model tests using shoulda matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,5 @@
-describe User do
+describe User, type: :model do
   let(:user) { create(:user) }
 
-  it 'should create a valid user with acceptable params' do
-    expect(user).to be_valid
-    expect(user).to be_a(User)
-  end
-
-  it 'should not create a user with unacceptable params' do
-    @bad_user = user
-    @bad_user.name = nil
-    @bad_user.provider = nil
-
-    expect(@bad_user).to_not be_valid
-  end
-
-  it '#name returns a string' do
-    expect(user.name).to match 'Test User'
-  end
+  it { should validate_presence_of(:name) }
 end
